3d014c429e139e8998c61002c455ddd27fdd4408
swapniltandle <swapnil.tandle3@gmail.com>
2012-05-29 12:09:23 +0530

    committtt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAppCucumber::Application.config.secret_token = '25d22d4cf1167405576224bb921ca0f8756bf1daaa0148b02c37769847a58b3b77ff79c42d68cc13a41768111ab2ede4b4feb2213787ccf17c0944fc2bebe114'
